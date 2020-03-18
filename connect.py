b00b6151b4da9d80727431488ae873bf9e604a9e
Davide Barasti <dbarasti@gmail.com>
2020-03-18 19:48:49 +0100

    update with basic connection script

--- connect.py
+++ connect.py
@@ -1,27 +1,12 @@
-from wifi import Cell, Scheme
+import os
+from dotenv import load_dotenv
 
-def wifiscan():
+load_dotenv()
+SSID = os.getenv('SSID')
+PSW = os.getenv('SSID_PSW')
 
-    allSSID = Cell.all('wlan0')
-    print (allSSID) # prints all available WIFI SSIDs
-    myssid= 'Cell(ssid=davide-ThinkPad)' # vivekHome is my wifi name
-    
-    print(allSSID[myssid])
+def try_connection():
+    os.system(f'nmcli device wifi con "{SSID}" password "{PSW}"')
 
-    for i in range(len(allSSID)):
-        if str(allSSID [i]) == myssid:
-            a = i
-            myssidA = allSSID [a]
-            print (b)
-            break
-        else:
-            print ("getout")
 
-    # Creating Scheme with my SSID.
-    myssid= Scheme.for_cell('wlan0', 'accespoint', myssidA, '3ze7GvdI') # vive1234 is the password to my wifi myssidA is the wifi name 
-
-    print (myssid)
-    myssid.save()
-    myssid.activate()
-
-wifiscan()   
+try_connection()
